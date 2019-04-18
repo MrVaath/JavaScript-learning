de3641cb11ba5371ad0fac54c8f9657791338f29
MrVaath <pat.wylegala@gmail.com>
2019-04-18 18:12:26 +0200

    Operators, mutation. typeof

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,3 +1,8 @@
+/*
+Variables and data types
+*/
+
+/*
 var firstName = 'John';
 console.log(firstName);
 
@@ -13,5 +18,62 @@ console.log(job);
 job = 'Teacher';
 console.log(job);
 
+// Variable naming rules
 var _3years = 3;
 var johnMark = 'John and Mark';
+*/
+
+
+/*
+Variable mutation and type coercion
+ */
+/*
+ var firstName = 'John';
+ var age = 28;
+
+ // Type coercion
+ console.log(firstName + ' ' + age);
+ 
+ var job, isMarried;
+ job = 'teacher';
+ isMarried = false;
+
+ console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+ 
+ // Variable mutation
+ age = 'twenty eight';
+ job = 'driver';
+
+ alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
+*/
+
+/*
+Basic operators
+*/
+
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 10);
+
+// Logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+// typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+console.log(typeof x);
