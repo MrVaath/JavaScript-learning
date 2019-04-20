e776b1a5233efec682cae6bf4d07b0f403d863de
MrVaath <pat.wylegala@gmail.com>
2019-04-20 16:45:58 +0200

    Bind, Call and Apply

--- 4-advanced-JS/script.js
+++ 4-advanced-JS/script.js
@@ -176,7 +176,7 @@ game();
 */
 
 // CLOSURES
-
+/*
 function retirement(retirementAge) {
   var a = ' years left until retirement';
   return function(yearOfBirth) {
@@ -208,3 +208,78 @@ function interviewQuestion(job) {
 }
 
 interviewQuestion('teacher')('John');
+*/
+
+// BIND, CALL AND APPLY
+
+var john = {
+  name: 'John',
+  age: 26,
+  job: 'teacher',
+  presentation: function(style, timeOfDay) {
+    if (style === 'formal') {
+      console.log(
+        'Good ' +
+          timeOfDay +
+          ", Ladies and Gentlemen! I'm " +
+          this.name +
+          ", I'm a " +
+          this.job +
+          " and I'm " +
+          this.age +
+          ' years old'
+      );
+    } else if (style === 'friendly') {
+      console.log(
+        "Hey! What's up? I'm " +
+          this.name +
+          ", I'm a " +
+          this.job +
+          " and I'm " +
+          this.age +
+          ' years old. Have a nice ' +
+          timeOfDay
+      );
+    }
+  }
+};
+var emily = {
+  name: 'Emily',
+  age: 35,
+  job: 'designer'
+};
+
+john.presentation('formal', 'morning');
+john.presentation.call(emily, 'friendly', 'afternoon');
+// john.presentation.apply(emily, ['friendly', 'afternoon']);
+
+var johnFriendly = john.presentation.bind(john, 'friendly');
+
+johnFriendly('morning');
+johnFriendly('night');
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+emilyFormal('afternoon');
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
+
+function calculateAge(el) {
+  return 2019 - el;
+}
+
+function isFullAge(limit, el) {
+  return el >= limit;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+console.log(ages);
+console.log(fullJapan);
